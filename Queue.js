964dd731e06343852a1e8daf9d7e47aea6c9e52f
Maxim Moskalenko <MaksM.84@gmail.com>
2020-07-13 13:30:21 +0300

    Queue

--- Queue.js
+++ Queue.js
@@ -1,90 +1,91 @@
-// Queue is an abstract data type FIFO (first in - first out).
-// Очередь - абстрактный тип данных (первый пришёл - первый вышел).
-
 class QueueNode {
   constructor(value) {
-    this.value = value;
-    this.next = null;
+    this._value = value;
+    this._next = null;
   }
 }
 
 class Queue {
   constructor() {
-    this.first = null;
-    this.last = null;
-    this.count = 0;
+    this._first = null;
+    this._last = null;
+    this._size = 0;
   }
 
-  size() {
-    return this.count;
+  get size() {
+    return this._size;
   }
 
   isEmpty() {
-    return !this.first;
-  }
-
-  data() {
-    if (this.first === null) {
-      console.log('Queue is empty!');
-    } else {
-      const arrQueue = [];
-      function logData(dataQueue) {
-        if (dataQueue !== null) {
-          arrQueue.push(dataQueue.value);
-          logData(dataQueue.next);
-        }
-      }
-      logData(this.first);
-      console.log('Queue: ', arrQueue);
-    }
+    return this.size === 0;
   }
 
   enqueue(value) {
-    const oldLast = this.last;
-    this.last = new QueueNode(value);
+    const oldLast = this._last;
+    this._last = new QueueNode(value);
+
     if (this.isEmpty()) {
-      this.first = this.last;
+      this._first = this._last;
     } else {
-      oldLast.next = this.last;
+      oldLast._next = this._last;
     }
-    this.count += 1;
+    this._size += 1;
     return value;
   }
 
   dequeue() {
     if (this.isEmpty()) {
-      this.last = null;
+      this._last = null;
       return null;
     }
-    let deletedValue = this.first.value;
-    this.first = this.first.next;
-    this.count -= 1;
+    let deletedValue = this._first._value;
+    this._first = this._first._next;
+    this._size -= 1;
     return deletedValue;
+  }
+
+  data() {
+    if (this._first === null) {
+      console.log('Queue: empty!');
+      return;
+    }
+    let arrQueue = [];
+    function logData(dataQueue) {
+      if (dataQueue !== null) {
+        arrQueue.push(dataQueue._value);
+        logData(dataQueue._next);
+      }
+    }
+    logData(this._first);
+    console.log('Queue: ');
+    arrQueue.forEach(node => console.log(node));
+    arrQueue = null;
   }
 }
 
-Object.prototype.toString = () => 'Object JavaScript';
-Array.prototype.toString = () => 'Array JavaScript';
-
-const myQueue = new Queue();
-console.log(`FIRST   IN -> ${myQueue.enqueue(1)}`);
-console.log(`SECOND  IN -> ${myQueue.enqueue(true)}`);
-console.log(`THIRD   IN -> ${myQueue.enqueue(null)}`);
-console.log(`FOURTH  IN -> ${myQueue.enqueue(undefined)}`);
-console.log(`FIFTH   IN -> ${myQueue.enqueue('simple string')}`);
-console.log(`SIXTH   IN -> ${myQueue.enqueue({})}`);
-console.log(`SEVENTH IN -> ${myQueue.enqueue([])}`);
-console.log(`EIGHTH  IN -> ${myQueue.enqueue(function() {})}`);
-console.log();
-myQueue.data();
-console.log();
-console.log(`FIRST   OUT -> ${myQueue.dequeue()}`);
-console.log(`SECOND  OUT -> ${myQueue.dequeue()}`);
-console.log(`THIRD   OUT -> ${myQueue.dequeue()}`);
-console.log(`FOURTH  OUT -> ${myQueue.dequeue()}`);
-console.log(`FIFTH   OUT -> ${myQueue.dequeue()}`);
-console.log(`SIXTH   OUT -> ${myQueue.dequeue()}`);
-console.log(`SEVENTH OUT -> ${myQueue.dequeue()}`);
-console.log(`EIGHTH  OUT -> ${myQueue.dequeue()}`);
-console.log();
-myQueue.data();
+const queue = new Queue();
+console.log(`1 in -> ${queue.enqueue(1)}`);
+console.log(`2 in -> ${queue.enqueue(true)}`);
+console.log(`3 in -> ${queue.enqueue(null)}`);
+console.log(`4 in -> ${queue.enqueue(undefined)}`);
+console.log(`5 in -> ${queue.enqueue('string')}`);
+console.log(`6 in -> ${queue.enqueue({ name: 'object' })}`);
+console.log(`7 in -> ${queue.enqueue([1, 2, 3])}`);
+console.log(`8 in -> ${queue.enqueue(function func() {})}`);
+console.log('-'.repeat(25));
+queue.data();
+console.log('-'.repeat(25));
+console.log('размер очереди:', queue.size);
+console.log('-'.repeat(25));
+console.log(`1 out -> ${queue.dequeue()}`);
+console.log(`2 out -> ${queue.dequeue()}`);
+console.log(`3 out -> ${queue.dequeue()}`);
+console.log(`4 out -> ${queue.dequeue()}`);
+console.log(`5 out -> ${queue.dequeue()}`);
+console.log(`6 out -> ${queue.dequeue()}`);
+console.log(`7 out -> ${queue.dequeue()}`);
+console.log(`8 out -> ${queue.dequeue()}`);
+console.log('-'.repeat(25));
+queue.data();
+console.log('-'.repeat(25));
+console.log('размер очереди:', queue.size);

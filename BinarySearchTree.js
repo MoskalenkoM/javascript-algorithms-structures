0514dff9852fa332f1119a5961ecfe445a51ab46
Maxim Moskalenko <MaksM.84@gmail.com>
2020-07-18 16:51:37 +0300

    Need refactoring

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -28,9 +28,9 @@ class BST {
     return this._root === null;
   }
 
-  contains(key) {
-    if(key === null) {
-      return false;
+  hasNode(key) {
+    if(key === undefined) {
+      return new Error('Method "hasNode" has not "key" for checking!');
     }
     return this.getNode(key) !== null;
   }
@@ -39,52 +39,56 @@ class BST {
     if(node === null) {
       return null;
     }
-    if(key === undefined) {
-      return new Error('Key is undefined!');
-    }
-
     if(key < node._key) {
       return this._itGetNode(node._left, key);
-    }
-    if(key > node._key) {
+    } else if(key > node._key) {
       return this._itGetNode(node._right, key);
-    }
-    if(key === node._key) {
+    } else {
       return node._value;
     }
   }
 
   getNode(key) {
+    if(key === undefined) {
+      return new Error('Method "getNode" has not "key" for checking!');
+    }
     return this._itGetNode(this._root, key);
   }
 
   _itSetNode(x, key, value) {
     if(x === null) {
+      /* first node */
       return new BSTNode(key, value, 1);
     }
 
     if(key < x._key) {
+      /* go to left */
       x._left = this._itSetNode(x._left, key, value);
     } else if(key > x._key) {
+      /* go to right */
       x._right = this._itSetNode(x._right, key, value);
     } else {
+      /* set value */
       x._value = value;
     }
 
-    x._size = 1 + this._itSize(x._left) + this._itSize(x._right);
+    /* increasing size */
+    x._size = this._itSize(x._left) + this._itSize(x._right) + 1;
 
+    /* return node */
     return x;
   }
 
   setNode(key, value) {
     if(key === undefined || value === undefined) {
-      return new Error('Method SetNode has no input parameters key or value!');
+      return new Error('Method "SetNode" has no input parameters "key" or "value"!');
     }
-    if(key === null) {
-      // delete node
+    if(value === null) {
+      // TODO: delete node
       return;
     }
     this._root = this._itSetNode(this._root, key, value);
+    /* TODO: return below just for console.log */
     return !!this._root;
   }
 }
@@ -98,5 +102,5 @@ console.log('add 7', bst.setNode(7, 'seven'));
 console.log('add -2', bst.setNode(-2, 'minus two'));
 console.log('bst size:', bst.size());
 console.log('bst is empty:', bst.isEmpty());
-console.log('bst has 7:', bst.contains(7));
-console.log('bst has 3:', bst.contains(3));
+console.log('bst has 7:', bst.hasNode(7));
+console.log('bst has 3:', bst.hasNode(3));
